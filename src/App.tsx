8be56a92d8083a5e40a7bce39ce38b1421386748
Jakub Adamczyk <jkk.adamczyk@gmail.com>
2019-04-27 13:11:45 +0200

    add boxes

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,21 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.scss';
+import React, { Component } from "react";
+import "./App.scss";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        
+        <div className="half-window" style={{ backgroundColor: "purple" }}>
+          <div
+            className="horizontal-window"
+            style={{ backgroundColor: "red" }}
+          />
+          <div
+            className="horizontal-window"
+            style={{ backgroundColor: "brown" }}
+          />
+        </div>
+        <div className="half-window" style={{ backgroundColor: "yellow" }} />
       </div>
     );
   }
