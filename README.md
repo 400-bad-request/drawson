30655c0840267c6c517f1bc34cf0ff2dce5acf15
Jakub Adamczyk <jkk.adamczyk@gmail.com>
2019-05-11 21:20:17 +0200

    add deferred change action

--- README.md
+++ README.md
@@ -2,6 +2,12 @@
 
 Code playground
 
+## Patron
+
+![Saint Compilatio](https://pbs.twimg.com/profile_images/1050406689890062338/J5M_EZOx_400x400.jpg)
+
+Saint Compilatio, patron of our compilator 🙏
+
 ## Check out online
 [Netlify link](https://focused-bohr-0e85d0.netlify.com)
 
@@ -32,18 +38,3 @@ Your app is ready to be deployed!
 
 See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
 
-### `yarn run eject`
-
-**Note: this is a one-way operation. Once you `eject`, you can’t go back!**
-
-If you aren’t satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
-
-Instead, it will copy all the configuration files and the transitive dependencies (Webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you’re on your own.
-
-You don’t have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn’t feel obligated to use this feature. However we understand that this tool wouldn’t be useful if you couldn’t customize it when you are ready for it.
-
-## Learn More
-
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
-
-To learn React, check out the [React documentation](https://reactjs.org/).
--- src/components/CodeEditor/CodeEditor.tsx
+++ src/components/CodeEditor/CodeEditor.tsx
@@ -1,42 +1,60 @@
-import React, {FunctionComponent} from "react";
-import AceEditor from "react-ace";
-import "./CodeEditor.scss";
+import React, { FunctionComponent } from 'react';
+import AceEditor from 'react-ace';
+import './CodeEditor.scss';
 
-import "brace/theme/monokai";
-import {AppState} from "../../store";
-import {connect} from "react-redux";
-import {updateCodeEditorContent} from "../../store/editor/actionCreators";
+import 'brace/theme/monokai';
+import { AppState } from '../../store';
+import { connect } from 'react-redux';
+import { updateCodeEditorContent } from '../../store/editor/actionCreators';
 
 interface IProps {
-    width: number;
-    height: number;
-    codeEditorContent: string;
-    updateCodeEditorContent: (codeEditorContent: string) => any;
+  width: number;
+  height: number;
+  codeEditorContent: string;
+  updateCodeEditorContent: (codeEditorContent: string) => any;
 }
 
-export const CodeEditorComponent = (props: IProps) => {
-    const onChange = (newValue: string) => {
-        props.updateCodeEditorContent(newValue);
-    };
+let timeout;
 
-    return (
-        <AceEditor
-            theme={"github"}
-            width={props.width + "px"}
-            height={props.height + "px"}
-            fontSize={16}
-            value={props.codeEditorContent}
-            onChange={onChange}
-        />
-    );
+export const CodeEditorComponent = (props: IProps) => {
+  const onChange = (newValue: string) => {
+    props.updateCodeEditorContent(newValue);
+  };
+
+  const deferredCompilation = () => {
+    if (timeout) {
+      clearTimeout(timeout);
+      timeout = setTimeout(() => {
+        console.log('test');
+      }, 3000);
+    } else {
+      timeout = setTimeout(() => {
+        console.log('test');
+      }, 3000);
+    }
+  };
+
+  return (
+    <AceEditor
+      theme={'github'}
+      width={props.width + 'px'}
+      height={props.height + 'px'}
+      fontSize={16}
+      value={props.codeEditorContent}
+      onChange={onChange}
+    />
+  );
 };
 
 const mapStateToProps = (state: AppState) => ({
-    codeEditorContent: state.editor.codeEditorContent
+  codeEditorContent: state.editor.codeEditorContent,
 });
 
 const dispatchToProps = {
-    updateCodeEditorContent: updateCodeEditorContent
+  updateCodeEditorContent: updateCodeEditorContent,
 };
 
-export const CodeEditor = connect(mapStateToProps, dispatchToProps)(CodeEditorComponent);
+export const CodeEditor = connect(
+  mapStateToProps,
+  dispatchToProps
+)(CodeEditorComponent);
